c671f3ac5c1e1c924e1d1f0b7528e6878afcef5a
felipe <felipe.cardoso@go-it.work>
2023-04-11 11:35:39 -0300

    imageid tests

---
+++ EyeD.UnitTests/FakeData/ImageIdFakeData.cs
@@ -0,0 +1,9 @@
+﻿using EyeD.Domain.ValueObjects;
+
+namespace EyeD.UnitTests.FakeData;
+
+internal sealed class ImageIdFakeData
+{
+    internal ImageId ImageIdInvalido = new("");
+    internal ImageId ImageIdValido = new("shcjkdjhsd-98h-d");
+}
---
+++ EyeD.UnitTests/ValueObjects/ImageIdTests.cs
@@ -0,0 +1,37 @@
+﻿using EyeD.Domain.ValueObjects;
+using EyeD.UnitTests.FakeData;
+
+namespace EyeD.UnitTests.ValueObjects;
+
+public sealed class ImageIdTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("")]
+    [InlineData("d")]
+    [InlineData("ds")]
+    [InlineData("sda")]
+    [InlineData("sdas")]
+    [InlineData("dsasd")]
+    [InlineData("sdasda")]
+    [InlineData("asdasda")]
+    [InlineData("asdasdas")]
+    [InlineData("asdasdasd")]
+    [InlineData("     ")]
+    [InlineData(StaticData.MAIS_DE_150_CHAR)]
+
+
+    public void ShouldReturnError_WhenImageId_IsInvalid(string imageId)
+    {
+        var image = new ImageId(imageId);
+        Assert.False(image.IsValid);
+    }
+
+
+    [Fact]
+    public void ShouldReturnSuccess_WhenImageId_IsValid()
+    {
+        var image = new ImageId("shdjdkfjhdy6-d");
+        Assert.True(image.IsValid);
+    }
+}
