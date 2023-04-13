e0d00405b2eedd381d1836c952c6137857b7aa2a
felipe <felipe.cardoso@go-it.work>
2023-04-13 12:43:43 -0300

    value objecets tests done

---
+++ EyeD.UnitTests/FakeData/SKUFakeData.cs
@@ -0,0 +1,10 @@
+﻿using EyeD.Domain.ValueObjects;
+
+namespace EyeD.UnitTests.FakeData;
+
+internal class SKUFakeData
+{
+    internal SKU InvalidSKU = new("");
+    internal SKU ValidSKU = new("djdhs-ksjdh");
+
+}
---
+++ EyeD.UnitTests/ValueObjects/SKUTests.cs
@@ -0,0 +1,31 @@
+﻿using EyeD.Domain.ValueObjects;
+using EyeD.UnitTests.FakeData;
+using Xunit;
+
+namespace EyeD.UnitTests.ValueObjects;
+
+public sealed class SKUTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("a")]
+    [InlineData("ss")]
+    [InlineData("sss")]
+    [InlineData("dsds")]
+    [InlineData("    ")]
+    [InlineData(StaticData.MAIS_DE_16_CHAR)]
+
+    public void ShouldReturnErrorWhen_SKU_isInvalid(string sku)
+    {
+        var s = new SKU(sku);
+        Assert.False(s.IsValid);
+    }
+
+    [Fact]
+    public void ShouldReturnSuccessWhen_SKU_isValid()
+    {
+        var sku = new SKU("djsldjddd-d");
+        Assert.True(sku.IsValid);
+    }
+
+}
