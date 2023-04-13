8210c5b57e0461ee2cc62dbd7b9fb15ee8742f2c
felipe <felipe.cardoso@go-it.work>
2023-04-13 09:49:50 -0300

    ipv4 tests

---
+++ EyeD.UnitTests/FakeData/IPV4FakeData.cs
@@ -0,0 +1,9 @@
+﻿using EyeD.Domain.ValueObjects;
+
+namespace EyeD.UnitTests.FakeData;
+
+internal sealed class IPV4FakeData
+{
+    internal IPV4 IPV4Invalido = new("dasddaadasdadadasdasdasdasdadsadasdadadasda");
+    internal IPV4 IPV4Valido = new("192.168.15.40");
+}
---
+++ EyeD.UnitTests/ValueObjects/IPV4Tests.cs
@@ -0,0 +1,29 @@
+﻿using EyeD.Domain.ValueObjects;
+using EyeD.UnitTests.FakeData;
+
+namespace EyeD.UnitTests.ValueObjects;
+
+public sealed class IPV4Tests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("a")]
+    [InlineData("ds")]
+    [InlineData("dsx")]
+    [InlineData("dsdf")]
+    [InlineData("     ")]
+    [InlineData(StaticData.MAIS_DE_150_CHAR)]
+    [InlineData(StaticData.MAIS_DE_16_CHAR)]
+    public void ShouldReturnError_WhenIPV4_isInvalid(string ipv4)
+    {
+        var ip = new IPV4(ipv4);
+        Assert.False(ip.IsValid);
+    }
+
+    [Fact]
+    public void ShouldReturnSuccess_WhenIPV4_isValid()
+    {
+        var ip = new IPV4("192.168.15.40");
+        Assert.True(ip.IsValid);    
+    }
+}
